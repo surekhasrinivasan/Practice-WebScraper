a23a6015297be15bbeae7182d8680939900e1554
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-06 19:49:09 -0500

    Created Stock.cs model class

---
+++ ScraperPractice/Models/Stock.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScraperPractice.Models
+{
+    public class Stock
+    {
+        public int ID { get; set; }
+        public string Symbol { get; set; }
+        public decimal LastPrice { get; set; }
+        public decimal Change { get; set; }
+        public decimal PercentChange { get; set; }
+        public string Currency { get; set; }
+        public decimal MarketCap { get; set; }
+        public DateTime MarketTime { get; set; }
+    }
+}
